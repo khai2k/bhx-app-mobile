41968dd44165425c51682d29c1245202b665022e
Nguyễn Tấn Ninh <nguyentanninh123@gmail.com>
2021-05-18 12:42:22 +0700

    Tối ưu performance

---
+++ app/components/Shared/navMenu/NavCateParent.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, Image, FlatList } from 'react-native';
+import { ImageNavMenu } from '../../../images';
+import { styles } from './styles';
+
+const NavCateParent = (props) => {
+    return (
+        <View style={styles.navLeft}>
+            <View style={styles.navLeftTop}>
+                <TouchableOpacity
+                    style={styles.btnClose}
+                    onPress={() => props.navigation.goBack()}>
+                    <Image
+                        style={styles.iconClose}
+                        source={ImageNavMenu.imgIconClose}
+                    />
+                    <Text style={styles.textBtnClose}>Đóng</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.btnHome}
+                    onPress={() => props.navigation.navigate('Main')}>
+                    <Image
+                        source={ImageNavMenu.imgIconHome}
+                        style={styles.iconHome}
+                    />
+                </TouchableOpacity>
+            </View>
+            <FlatList
+                style={styles.navLeftBottom}
+                data={props.masterData}
+                renderItem={(item) => {
+                    return (
+                        <RenderCateItem
+                            item={item}
+                            cateFilter={props.cateFilter}
+                            setCateFilter={props.setCateFilter}
+                        />
+                    );
+                }}
+            />
+        </View>
+    );
+};
+
+// Render danh sách cate cha
+const RenderCateItem = (props) => {
+    const { item } = props.item;
+
+    return (
+        <TouchableOpacity
+            style={[
+                styles.itemCate,
+                item.ReferenceId === props.cateFilter && styles.itemCateActive
+            ]}
+            onPress={() => {
+                props.setCateFilter(item.ReferenceId);
+            }}>
+            {item.ReferenceId === '-1' && (
+                <Image
+                    style={styles.iconPromotion}
+                    source={ImageNavMenu.imgIconPromotion}
+                />
+            )}
+            <Text style={styles.txtCate}>{item.Text}</Text>
+        </TouchableOpacity>
+    );
+};
+
+export default NavCateParent;
