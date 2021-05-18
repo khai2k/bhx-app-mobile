6fc1b18aaf7befe509349d4d4547192d168e4ddf
thanhquy3n <50368555+thanhquy3n@users.noreply.github.com>
2021-05-18 18:47:18 +0700

    input info cart

---
+++ app/container/cart/inputinfo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+export const inputinfo = (props) => {
+  return (
+    <View></View>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(inputinfo)
