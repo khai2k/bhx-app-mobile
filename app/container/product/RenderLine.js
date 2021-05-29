41fa70c25bb0996896231f5bd1093952d6139477
Nguyễn Doãn Mạnh <manhhuanhh@gmail.com>
2021-05-30 00:31:42 +0700

    update page home

--- app/container/product/RenderLine.js
+++ app/container/product/RenderLine.js
@@ -26,10 +26,9 @@ const RenderLine = (props) => {
     const [categoriesIdFresh, setCategoriesIdFresh] = useState([]); // list category id fresh
     const [categoryId, setCategoryId] = useState(); // category fresh id
     const [virtualChildCateIds, setVirtualChildCateIds] = useState([]); // child category fresh id
-    const [selectedId, setSelectedId] = useState(0);
-    const [isClickCateFresh, setIsClickCateFresh] = useState(false);
-    const [isClickViewmore, setIsClickViewmore] = useState(false);
-
+    const [selectedId, setSelectedId] = useState(0); // category fresh active
+    const [isClickCateFresh, setIsClickCateFresh] = useState(false); // check click category fresh
+    const [isClickViewmore, setIsClickViewmore] = useState(false); // check click xem them
     if (
         props.lineItem.CategoryId === 8686 &&
         props.lineItem.Categorys !== null &&
@@ -44,18 +43,15 @@ const RenderLine = (props) => {
     }
 
     useEffect(() => {
-        console.log('GetProductFreshCate');
         isClickCateFresh && GetProductFreshCate();
     }, [isClickCateFresh]);
 
     useEffect(() => {
-        console.log('GenMoreProduct');
         isClickViewmore && GenMoreProduct();
     }, [isClickViewmore]);
 
-    // Xem thêm sản phẩm của các line
-    function GenMoreProduct() {
-        // Chỉ lấy 9 productid mới nhất truyền vô excludeProductIds
+    // Chỉ lấy 9 productid mới nhất truyền vô excludeProductIds
+    function Get9ProductId() {
         const excludeProductIds = [];
         const get9LastProduct = products.slice(
             products.length - 9,
@@ -64,6 +60,12 @@ const RenderLine = (props) => {
         get9LastProduct?.map((item) => {
             return excludeProductIds.push(item.Id);
         });
+        return excludeProductIds;
+    }
+
+    // Xem thêm sản phẩm của các line
+    function GenMoreProduct() {
+        const excludeProductIds = Get9ProductId();
 
         const bodyApi = {
             token: '',
@@ -86,13 +88,11 @@ const RenderLine = (props) => {
         apiBase(API_CONST.GET_MORE_LIST_PRODUCT, METHOD.POST, bodyApi)
             .then((response) => {
                 console.log('GET_MORE_LIST_PRODUCT Data:', response);
+                setIsClickViewmore(false);
                 setProducts([...products, ...response.Value]);
                 if (response.OtherData?.TotalRest > 0) {
                     setPageIndex(pageIndex + 1);
                     setTotalProduct(response.OtherData.TotalRest);
-                } else {
-                    setPageIndex(1);
-                    setTotalProduct(0);
                 }
             })
             .catch((error) => {
@@ -101,6 +101,7 @@ const RenderLine = (props) => {
     }
 
     function GetProductFreshCate() {
+        const excludeProductIds = pageIndex > 0 ? Get9ProductId() : [];
         const bodyApi = {
             token: '',
             us: '',
@@ -110,7 +111,7 @@ const RenderLine = (props) => {
             storeId: 6463,
             data: {
                 ListProducts: '',
-                PageIndex: 0,
+                PageIndex: pageIndex,
                 PageSize: 9,
                 Phone: '',
                 CategoryIds:
@@ -118,7 +119,7 @@ const RenderLine = (props) => {
                     virtualChildCateIds.length > 0
                         ? virtualChildCateIds.toString()
                         : categoryId.toString(),
-                ExcludeProductIds: '',
+                ExcludeProductIds: excludeProductIds?.toString(),
                 CategoryId: categoryId,
                 ListCategoryIds: categoriesIdFresh?.toString()
             },
@@ -126,21 +127,24 @@ const RenderLine = (props) => {
         };
         apiBase(API_CONST.GET_FRESH_PRODUCTS, METHOD.POST, bodyApi)
             .then((response) => {
-                console.log('GET_MORE_LIST_PRODUCT Data:', response);
+                console.log('GET_FRESH_PRODUCTS Data:', response);
+                setIsClickCateFresh(false);
                 if (
                     response !== null &&
                     response.Value !== null &&
                     response.Value.length > 0
                 ) {
-                    setIsClickCateFresh(false);
-                    setProducts(response.Value);
-                    setTotalProduct(response.OtherData?.total);
+                    setPageIndex(pageIndex + 1);
+                    pageIndex > 0
+                        ? setProducts([...products, ...response.Value])
+                        : setProducts(response.Value);
+                    setTotalProduct(response.OtherData?.TotalRest);
                 } else {
                     Alert.alert('Kết quả', 'Không tìm thấy sản phẩm.');
                 }
             })
             .catch((error) => {
-                console.log('GET_MORE_LIST_PRODUCT Error:', error);
+                console.log('GET_FRESH_PRODUCTS Error:', error);
             });
     }
 
@@ -199,18 +203,21 @@ const RenderLine = (props) => {
                 {products !== null && products.length > 0 && totalProduct > 0 && (
                     <TouchableOpacity
                         onPress={() => {
-                            setIsClickViewmore(true);
-                            setPageIndex(1);
-                            // GenMoreProduct(props.lineItem.CategoryIds);
+                            selectedId > 0
+                                ? setIsClickCateFresh(true)
+                                : setIsClickViewmore(true);
+                            pageIndex <= 0 && setPageIndex(1);
                         }}
                         style={styles.viewmoreProduct}>
                         <View style={styles.viewmoreProduct_text}>
                             <Text style={styles.viewmoreProduct_total}>
                                 {`Xem thêm ${totalProduct} sản phẩm`}
                             </Text>
-                            <Text style={styles.viewmoreProduct_cateName}>
-                                {props.lineItem.Text.toLowerCase()}
-                            </Text>
+                            {selectedId <= 0 && (
+                                <Text style={styles.viewmoreProduct_cateName}>
+                                    {props.lineItem.Text.toLowerCase()}
+                                </Text>
+                            )}
                         </View>
                     </TouchableOpacity>
                 )}
